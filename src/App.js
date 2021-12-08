1a73f8655a17cd470e6b194eff702ae3d40e17fc
Pailynn Jucksriporn <pailynn@Pailynns-MacBook-Air.local>
2021-12-08 08:45:50 +0700

    chang href url

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://c4m0e7vhf4.execute-api.ap-southeast-1.amazonaws.com"
           target="_blank"
           rel="noopener noreferrer"
         >
