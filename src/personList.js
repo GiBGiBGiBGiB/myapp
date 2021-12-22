5b9cbf6a85ed63fde2e3f3726f4b96b2ccf5e4ff
Gibgib <pailynncvd@gmail.com>
2021-12-22 08:17:09 +0700

    22120817

--- src/personList.js
+++ src/personList.js
@@ -4,21 +4,18 @@ import axios from 'axios';
 export default function PersonList() {
   const [state,setState] = useState({Items:[]})
 
-  useEffect(()=>{
-    axios.get(`https://c4m0e7vhf4.execute-api.ap-southeast-1.amazonaws.com/items`)
-    .then(res => {
-      // console.log(res.data);
-      const items = res.data.Items;
-      setState( items );
-      console.log(state)
-    })
-    .catch((err)=>{console.error(err)})
+  useEffect(async()=>{
+    const result = await axios.get(`https://c4m0e7vhf4.execute-api.ap-southeast-1.amazonaws.com/items`)
+
+      setState( result.data );
+      console.log(result)
+
     },[]) 
 
     return (
       <ul>test
         {
-          state.map(item =>(
+          state.Items.map(item =>(
               <li>{item.ID};{item.LatestGreetingTime}</li>
             ))
         }
